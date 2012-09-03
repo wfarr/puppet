31537f372bd00e3404a087aa1319c33fcb36ea68
Gary Larizza <gary@puppetlabs.com>
2012-09-03 14:06:40 -0700

    Make directoryservice group provider ensurable
    This commit is the initial commit for the directoryservice group
    provider.  With this commit series, the provider is now ensurable.

--- lib/puppet/provider/group/directoryservice.rb
+++ lib/puppet/provider/group/directoryservice.rb
@@ -6,7 +6,9 @@ require 'pp'
 Puppet::Type.type(:group).provide :directoryservice do
   desc "Group management on OS X."
 
-  commands :dscl => "/usr/bin/dscl"
+  commands :dscl    => "/usr/bin/dscl"
+  commands :uuidgen => "/usr/bin/uuidgen"
+
   confine :operatingsystem => :darwin
   defaultfor :operatingsystem => :darwin
 
@@ -14,29 +16,28 @@ Puppet::Type.type(:group).provide :directoryservice do
   # This command creates methods that return @property_hash[:value]
   mk_resource_methods
 
+  has_feature :manages_members
+
   def self.ds_to_ns_attribute_map
     {
-      'RecordName' => :name,
-      'PrimaryGroupID' => :gid,
-      'NFSHomeDirectory' => :home,
-      'UserShell' => :shell,
-      'UniqueID' => :uid,
-      'RealName' => :comment,
-      'Password' => :password,
-      'GeneratedUID' => :guid,
-      'IPAddress'    => :ip_address,
-      'ENetAddress'  => :en_address,
+      'RecordName'      => :name,
+      'PrimaryGroupID'  => :gid,
+      'RealName'        => :comment,
+      'Password'        => :password,
+      'GeneratedUID'    => :guid,
       'GroupMembership' => :members,
+      'GroupMembers'    => :guid_list,
     }
   end
 
+  def self.ns_to_ds_attribute_map
+    @ns_to_ds_attribute_map ||= ds_to_ns_attribute_map.invert
+  end
+
   def self.instances
-    @array = []
-    get_all_groups.collect do |user|
-      @array << self.new(generate_attribute_hash(user))
+    get_all_groups.collect do |group|
+      self.new(generate_attribute_hash(group))
     end
-    pp @array
-    @array
   end
 
   def self.get_all_groups
@@ -52,7 +53,7 @@ Puppet::Type.type(:group).provide :directoryservice do
       case ds_to_ns_attribute_map[ds_attribute]
         when :members
           ds_value = ds_value
-        when :gid, :uid
+        when :gid
           # OS X stores objects like uid/gid as strings.
           # Try casting to an integer for these cases to be
           # consistent with the other providers and the group type
@@ -71,19 +72,93 @@ Puppet::Type.type(:group).provide :directoryservice do
     attribute_hash
   end
 
-  def exists?
-    if @property_hash[:ensure] == :present
-      true
-    else
-      false
+  #  Prefetching is necessary to use @property_hash inside any setter methods
+  def self.prefetch(resources)
+    instances.each do |prov|
+      if resource = resources[prov.name]
+        resource.provider = prov
+      end
     end
   end
 
-  def create
+##                   ##
+## Ensurable Methods ##
+##                   ##
 
+  def exists?
+    begin
+      dscl '.', 'read', "/Groups/#{@resource.name}"
+    rescue
+      return false
+    end
+    true
   end
 
-  def destroy
+  def create
+    dscl '.', '-create', "/Groups/#{@resource.name}"
 
+    # Generate a GUID for the new user
+    @guid = uuidgen
+
+    # Get an array of valid User type properties
+    valid_properties = Puppet::Type.type('Group').validproperties
+
+    # GUID is not a valid user type property, but since we generated it
+    # and set it to be @guid, we need to set it with dscl. To do this,
+    # we add it to the array of valid User type properties.
+    valid_properties.unshift(:guid)
+
+    # Iterate through valid User type properties
+    valid_properties.each do |attribute|
+      next if attribute == :ensure
+      value = @resource.should(attribute)
+
+      # Value defaults
+      if value.nil?
+        value = @guid if attribute == :guid
+        value = next_system_id if attribute == :gid
+      end
+
+      if value != '' and not value.nil?
+        begin
+          case attribute
+          when :guid
+            dscl '.', '-changei', "/Groups/#{@resource.name}", self.class.ns_to_ds_attribute_map[attribute], '1', @guid
+          when :members
+            value.each do |group|
+              dscl '.', '-merge', "/Groups/#{@resource.name}", 'GroupMembership', group
+            end
+          else
+            dscl '.', '-merge', "/Groups/#{@resource.name}", self.class.ns_to_ds_attribute_map[attribute], value
+          end
+        rescue => e
+         fail("Could not create #{@resource.class.name} #{@resource.name}: #{e.inspect}")
+        end
+      end
+    end
+  end
+
+  def delete
+    # This method is called when ensure => absent has been set.
+    # Deleting a user is handled by dscl
+    dscl '.', '-delete', "/Groups/#{@resource.name}"
+  end
+
+  ##                ##
+  ## Helper Methods ##
+  ##                ##
+
+  def next_system_id(min_id=20)
+    # Get the next available uid on the system by getting a list of user ids,
+    # sorting them, grabbing the last one, and adding a 1. Scientific stuff here.
+    dscl_output = dscl '.', '-list', '/Groups', 'gid'
+    # We're ok with throwing away negative uids here. Also, remove nil values.
+    user_ids = dscl_output.split.compact.collect { |l| l.to_i if l.match(/^\d+$/) }
+    ids = user_ids.compact!.sort! { |a,b| a.to_f <=> b.to_f }
+    # We're just looking for an unused id in our sorted array.
+    ids.each_index do |i|
+      next_id = ids[i] + 1
+      return next_id if ids[i+1] != next_id and next_id >= min_id
+    end
   end
 end
